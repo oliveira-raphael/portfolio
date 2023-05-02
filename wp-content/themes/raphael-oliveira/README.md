50fa37741f4fe1856af3dad5e23ca27fc3825dda
oliveira-raphael <74560897+oliveira-raphael@users.noreply.github.com>
2023-05-02 18:28:03 -0300

    Inicio portfólio

--- wp-content/themes/raphael-oliveira/README.md
+++
@@ -1,2 +0,0 @@
-# oltheme
-Tema desenvolvido pela OLWEB Solutions com o objetivo de atender com qualidade as necessidades de projetos de websites complexos.
--- wp-content/themes/raphael-oliveira/searchform.php
+++
@@ -1,6 +0,0 @@
-<form action="<?php echo get_site_url() ?>" method="get" accept-charset="utf-8" id="searchform" role="search">
-    <div class="searchform">
-        <input type="text" name="s" id="s" value="<?php the_search_query(); ?>" />
-        <button type="submit"><i class="fa fa-search"></i></button>
-    </div>
-</form>
